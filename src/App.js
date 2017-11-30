4748d84e9c46bd108ba416e421b9c28baefffb2b
leon-good-life <leon.good.life@gmail.com>
2017-11-30 11:05:40 +0200

    Remove unneeded files and code that auto generated by create-react-app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as booksActions from './actions/books';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   componentDidMount() {
@@ -23,15 +21,7 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div />
     );
   }
 }
